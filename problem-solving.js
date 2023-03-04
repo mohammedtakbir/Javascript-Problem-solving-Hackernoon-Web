441707a2eeb8eabf651ed71df1bf329cee251b40
mohammedtakbir <takbirhossain678@gmail.com>
2023-03-04 20:11:38 +0600

    problem solved: 6 and 6

--- problem-solving.js
+++ problem-solving.js
@@ -44,3 +44,20 @@
 //         console.log(`${num} X ${i} = ${table}`);
 //     }
 // }
+
+//* problem-05
+//? Calculate the sum of numbers from 1 to 10
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//     sum += i;
+// }
+// console.log(sum)
+
+//* problem-06
+//? Calculate 10!
+let factorial = 1;
+for(let i = 10; i>=1; i--){
+    let temp = factorial;
+    factorial *= i;
+    console.log(`${i} * ${temp} = ${factorial}`)
+}
