700b026709b5cb66cd8f9fb4948516ac5bef0488
mohammedtakbir <takbirhossain678@gmail.com>
2023-03-04 19:33:18 +0600

    problem:1-2 solved

---
+++ problem-solving.js
@@ -0,0 +1,18 @@
+//* problem-01
+//? Print numbers from 1 to 10
+// let i = 1;
+// while(i<11){
+//     console.log(i)
+//     i++;
+// }
+// for(let i = 1; i<=10; i++){
+//     console.log(i)
+// }
+
+//* problem-02
+//? print the odd numbers less than 100
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 !== 0) {
+        console.log(i)
+    }
+}
