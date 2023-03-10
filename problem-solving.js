a2981856dd34603e4028eed7b4efbd25edf1815b
mohammedtakbir <takbirhossain678@gmail.com>
2023-03-10 12:53:34 +0600

    get first 100 prime numbersa

--- problem-solving.js
+++ problem-solving.js
@@ -235,28 +235,29 @@ const positiveIntegerNumber = 357955;
 //* problem-19
 //? Print the first 100 prime numbers
 
-function prime(num) {
-    let count = 0;
-    let i = 0;
-    while (count < num) {
-        if (getPrimeNumber(i)) {
-            console.log(i);
-            count++;
-        }
-        i++;
-    }
-}
-function getPrimeNumber(num) {
-    if (num < 2) {
-        return false;
-    }
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-}
+// const getPrimeNumbers = (n) => {
+//     let count = 0;
+//     let i = 0;
+//     while (count < n) {
+//         if (isPrimeNumber(i)) {
+//             console.log(i);
+//             count++;
+//         }
+//         i++;
+//     }
+// }
 
-prime(100);
+// function isPrimeNumber(n) {
+//     if (n < 2) {
+//         return false;
+//     } else if (n === 2) {
+//         return true;
+//     }
+//     for (let i = 2; i < n; i++) {
+//         if (n % i === 0) {
+//             return false;
+//         }
+//     };
+//     return true;
+// }
+// console.log(getPrimeNumbers(9))
