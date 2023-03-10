0c363b366248fe4bfa28294156e97cd3e34f8ce5
mohammedtakbir <takbirhossain678@gmail.com>
2023-03-10 13:07:21 +0600

    Create a function that will return in an array the first nPrimes prime numbers greater than a particular number startAt

--- problem-solving.js
+++ problem-solving.js
@@ -210,7 +210,7 @@
 
 //* problem-18
 //? Calculate the sum of digits of a positive integer number
-const positiveIntegerNumber = 357955;
+// const positiveIntegerNumber = 357955;
 // function sum(num) {
 //     const digit = num.toString();
 //     const digitArr = digit.split('');
@@ -261,3 +261,33 @@ const positiveIntegerNumber = 357955;
 //     return true;
 // }
 // console.log(getPrimeNumbers(9))
+
+//* problem-20
+//? Create a function that will return in an array the first "nPrimes" prime numbers greater than a particular number "startAt"
+
+// function getPrimes(nPrime, startAt) {
+//     let count = 0;
+//     let i = startAt;
+//     while (count < nPrime) {
+//         if (isPrime(i)) {
+//             console.log(i);
+//             count++;
+//         }
+//         i++;
+//     }
+// }
+
+// function isPrime(n) {
+//     if (n < 2) {
+//         return false;
+//     } else if (n === 2) {
+//         return true;
+//     }
+//     for (let i = 2; i < n; i++) {
+//         if (n % i === 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// console.log(getPrimes(10, 359))
